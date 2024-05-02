a2cff4a3bfb09a8874a2400074ba4e5c7ab3693c
milkiascha7 <mikehailu504@gmail.com>
2024-05-02 09:39:09 -0600

    initial commit with assets

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <link rel="icon" href="/assets/brainwave-symbol.svg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
