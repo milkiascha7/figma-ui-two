62e117ff764c7e4fe44b56bd2330b78ec8752910
milkiascha7 <mikehailu504@gmail.com>
2024-05-22 10:27:39 -0700

    "collaboration section"

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import Button from "@/components/Button";
+
 import Benefits from "@/components/benefits/benefits";
+import Collaboration from "@/components/collaboration/collaboration";
 import Header from "@/components/header/Header";
 import Hero from "@/components/section/Hero";
 import ButtonGradient from "@/public/assets/svg/ButtonGradient";
@@ -7,12 +9,14 @@ import ButtonGradient from "@/public/assets/svg/ButtonGradient";
 export default function Home() {
   return (
     <>
-      <ButtonGradient />
-      <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
+      <ButtonGradient />
+
     </>
   );
 }
