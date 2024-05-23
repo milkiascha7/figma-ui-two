5892e16f66d27c82852342652157a2986118b176
milkiascha7 <mikehailu504@gmail.com>
2024-05-23 10:48:28 -0700

    "services section"

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Benefits from "@/components/benefits/benefits";
 import Collaboration from "@/components/collaboration/collaboration";
 import Header from "@/components/header/Header";
 import Hero from "@/components/section/Hero";
+import Services from "@/components/services/services";
 import ButtonGradient from "@/public/assets/svg/ButtonGradient";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
 
